eff46114dafbbef2034459f7788ffe138da17980
Alhassan Mariam <alhassanmaryam2017@gmail.com>
2022-10-19 16:00:10 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * pr8nt alpabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = la; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar(\n);
+	}
+}
