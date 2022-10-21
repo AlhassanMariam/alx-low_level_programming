5f334c0bb740c4d92a8fb04d6e070ec98a22d13d
Alhassan Mariam <alhassanmaryam2017@gmail.com>
2022-10-21 15:38:29 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,6 +12,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
 	/**
 	 *  while (i < 10)
 	 *  {
@@ -20,3 +21,4 @@ int main(void)
 	 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
+}
