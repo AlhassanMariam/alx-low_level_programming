0c412342258c60af3fdd50c40b521470722e61f7
Alhassan Mariam <alhassanmaryam2017@gmail.com>
2022-10-25 14:04:45 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse followed by a new line
+ * @s - string to print
+ */
+void print_rev(char *s)
+{
+	int i, m;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		m = (int) s[i];
+		_putchar(m);
+	}
+	_putchar(10);
+}
+
