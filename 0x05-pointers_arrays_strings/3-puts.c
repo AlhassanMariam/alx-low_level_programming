c9d123a3cfa9b33f2a689ebe8c8c9d66b6e5adb6
Alhassan Mariam <alhassanmaryam2017@gmail.com>
2022-10-25 10:57:00 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _prints - prints a string
+ * @str: string to be pointed
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i;
+	int s;
+
+	for (i = 0; str[i]; i++)
+	{
+		s = (int) str[i];
+		putchar(s);
+	}
+	_putchar(10);
+}
+
