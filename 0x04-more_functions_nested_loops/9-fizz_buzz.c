ec4e8a84ad18bd8ffa4620216e318e0bd9d31b84
Alhassan Mariam <alhassanmaryam2017@gmail.com>
2022-10-29 19:12:38 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - print fizzBuzz
- * Return: always 0
+ * main - print fizzbuzz
+ * Return: Always 0
  */
 int main(void)
 {
@@ -13,7 +13,7 @@ int main(void)
 	{
 		printf(" ");
 		if (n % 3 == 0)
-			printf("fizz");
+			printf("Fizz");
 		if (n % 5 == 0)
 			printf("Buzz");
 		if (n % 3 != 0 && n % 5 != 0)
